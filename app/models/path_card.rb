ec1c3651e50e561f9ca943e1e2486e114b0e346e
Austen Higgins-Cassidy <plasmarobo@gmail.com>
2014-09-09 21:57:45 -0400

    Additional Image resources

--- app/models/path_card.rb
+++ app/models/path_card.rb
@@ -1,2 +1,71 @@
 class PathCard < ActiveRecord::Base
+
+  def right_icon_tag
+    img = ""
+    case self.right_link
+    when "in"
+      img = "left64.png"
+    when "out"
+      img = "right64.png"
+    when "both"
+      img = "leftright64.png"
+    when "blocked"
+      img = "blocked64.png"
+    else
+      img = "error.png"
+    end
+    image_tag(img)  
+  end
+
+  def left_icon_tag
+    img = ""
+    case self.left_link
+    when "in"
+      img = "right64.png"
+    when "out"
+      img = "left64.png"
+    when "both"
+      img = "leftright64.png"
+    when "blocked"
+      img = "blocked64.png"
+    else
+      img = "error.png"
+    end
+    image_tag(img)
+  end
+
+  def up_icon_tag
+    img = ""
+    case self.up_link
+    when "in"
+      img = "down64.png"
+    when "out"
+      img = "up64.png"
+    when "both"
+      img = "updown64.png"
+    when "blocked"
+      img = "blocked64.png"
+    else
+      img = "error.png"
+    end
+    image_tag(img)
+  end
+
+  def down_icon_tag
+    img = ""
+    case self.down_link
+    when "in"
+      img = "up64.png"
+    when "out"
+      img = "down64.png"
+    when "both"
+      img = "updown64.png"
+    when "blocked"
+      img = "blocked64.png"
+    else
+      img = "error.png"
+    end
+    image_tag(img)
+  end
+
 end
