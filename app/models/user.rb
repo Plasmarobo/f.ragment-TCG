9c545db70d2172f1e848644dfd327becb911cd11
Plasmarobo <plasmarobo@gmail.com>
2014-09-15 16:11:39 -0400

    Authentication improvements

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,25 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates_uniqueness_of :name, case_sensistive: false
   has_many :players
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.name || self.email
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
 end
